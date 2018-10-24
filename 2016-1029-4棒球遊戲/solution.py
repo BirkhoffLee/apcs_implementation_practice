016aaddb162e50b369382b709a18d6b510567cfb
Birkhoff Lee <admin@birkhoff.me>
2018-10-24 20:50:56 +0800

    final solution

--- 2016-1029-4棒球遊戲/solution.py
+++ 2016-1029-4棒球遊戲/solution.py
@@ -12,7 +12,6 @@ rounds = [ k for i in list(map(list, zip(*lines))) for k in i ]
 nowPlayer = [1, 0, 0, 0]
 outCounter = 0
 totalScore = 0
-m = 0
 
 for idx, el in enumerate(rounds):
   if el == "1B":
@@ -36,32 +35,12 @@ for idx, el in enumerate(rounds):
     nowPlayer[2] = 0
     nowPlayer[1] = 0
   elif el == "FO" or el == "GO" or el == "SO":
-    maxOutTime -= 1
-    m += 1
-    # outCounter += 1
+    outCounter += 1
     
-    # if outCounter == maxOutTime:
-    #   break
+    if outCounter == maxOutTime:
+      break
 
-    # if outCounter % 3 == 0:
-    #   nowPlayer[3] = nowPlayer[2] = nowPlayer[1] = 0
-  if m == 3:
-    nowPlayer[3] = nowPlayer[2] = nowPlayer[1] = 0
-    m = 0
-  if maxOutTime == 0: break
-
+    if outCounter % 3 == 0:
+      nowPlayer[3] = nowPlayer[2] = nowPlayer[1] = 0
 
 print(totalScore)
-
-"""
-    0  1  2  3  4
-0   1B 1B FO GO 1B
-1   1B 2B FO FO SO
-2   SO HR SO 1B
-3   FO FO FO HR
-4   1B 1B 1B 1B
-5   GO GO 3B GO
-6   1B GO GO SO
-7   SO GO 2B 2B
-8   3B GO GO FO
-"""
