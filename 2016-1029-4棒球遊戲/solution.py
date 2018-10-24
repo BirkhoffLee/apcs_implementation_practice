19711ad29fdcb42fb275ec54d6dca632c6ba6e13
Birkhoff Lee <admin@birkhoff.me>
2018-10-24 20:42:13 +0800

    95% score solution

--- 2016-1029-4棒球遊戲/solution.py
+++ 2016-1029-4棒球遊戲/solution.py
@@ -9,70 +9,47 @@ for idx, line in enumerate(lines):
 
 rounds = [ k for i in list(map(list, zip(*lines))) for k in i ]
 
-# for i in range(1, totalRoundAmount + 1):
-#   if i > 9:
-#     if i % 9 == 0:
-#       x, y = (i - 1) // 9, 8
-#     else:
-#       x, y = i // 9, i % 9 - 1
-#   elif i == 9:
-#     x, y = 0, 8
-#   else:
-#     x, y = 0, i % 9 - 1
-
-#   # print("i = {}, cord ({}, {})".format(i, x, y))
-
-#   rounds.append(lines[y][x])
-
-# print(rounds)
-
-####
-
 nowPlayer = [1, 0, 0, 0]
 outCounter = 0
 totalScore = 0
-
-def score(playerList, N):
-  newList = playerList[:] # deepcopy
-  
-  for idx, el in enumerate(playerList):
-    if el == 0: continue
-
-    if idx + N == 4:
-      finalIdx = 0
-    else:
-      finalIdx = (idx + N) % 4
-    
-    # if finalIdx == 0:
-    #   # 球員回到本壘，得分
-    #   global totalScore
-    #   totalScore += 1
-
-    newList[finalIdx] = 1
-
-  return newList
+m = 0
 
 for idx, el in enumerate(rounds):
   if el == "1B":
     totalScore += nowPlayer[3]
-    nowPlayer = score(nowPlayer, 1)
+    nowPlayer[3] = nowPlayer[2] 
+    nowPlayer[2] = nowPlayer[1]
+    nowPlayer[1] = 1
   elif el == "2B":
     totalScore += nowPlayer[3] + nowPlayer[2]
-    nowPlayer = score(nowPlayer, 2)
+    nowPlayer[3] = nowPlayer[1] 
+    nowPlayer[2] = 1
+    nowPlayer[1] = 0
   elif el == "3B":
     totalScore += nowPlayer[3] + nowPlayer[2] + nowPlayer[1]
-    nowPlayer = score(nowPlayer, 3)
+    nowPlayer[3] = 1
+    nowPlayer[2] = 0
+    nowPlayer[1] = 0
   elif el == "HR":
-    totalScore += nowPlayer[3] + nowPlayer[2] + nowPlayer[1] + nowPlayer[0] + 1 # 全壘打，得分
-    nowPlayer = score(nowPlayer, 4)
+    totalScore += nowPlayer[3] + nowPlayer[2] + nowPlayer[1] + 1
+    nowPlayer[3] = 0
+    nowPlayer[2] = 0
+    nowPlayer[1] = 0
   elif el == "FO" or el == "GO" or el == "SO":
-    outCounter += 1
-
-    if outCounter == maxOutTime:
-      break
+    maxOutTime -= 1
+    m += 1
+    # outCounter += 1
     
-    if outCounter % 3 == 0:
-      nowPlayer = [1, 0, 0, 0] # reset
+    # if outCounter == maxOutTime:
+    #   break
+
+    # if outCounter % 3 == 0:
+    #   nowPlayer[3] = nowPlayer[2] = nowPlayer[1] = 0
+  if m == 3:
+    nowPlayer[3] = nowPlayer[2] = nowPlayer[1] = 0
+    m = 0
+  if maxOutTime == 0: break
+
 
 print(totalScore)
 
